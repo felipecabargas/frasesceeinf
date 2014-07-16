471ddeea81d0d540dcddadc61b21ddc77a4412c3
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2014-07-15 21:13:07 -0400

    Add logic on null phrases

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-    @phrase = Phrase.order('RANDOM()').last
+    if Phrase.count == 0
+      puts 'Hola'
+    else
+      @phrase = Phrase.order('RANDOM()').last
+    end
     @authors = User.all
   end
 
